2074f862a0d0d25cf1182d5e18845fced2a8a3a4
jjcm <j@jjcm.org>
2021-01-06 21:35:22 +1100

    more setup of formats. Need to round to even resolutions

--- route/encode.go
+++ route/encode.go
@@ -70,7 +70,7 @@ func Encode(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	nativeSize := math.Max(x, y)
-	aspectRatio := x / y
+	aspectRatio := y / x
 	fmt.Println(nativeSize)
 	fmt.Println(aspectRatio)
 
@@ -83,13 +83,52 @@ func Encode(w http.ResponseWriter, r *http.Request) {
 	// For bitrates, we use 2 * the number of pixels
 	// I.e. 8k has 33 million pixels, so we use 66Mbps
 	nativeBitrate := "2M"
+	downscaleResolution := ""
 
 	// Halfway between 720p and 480p
 	// ( 1280 + 854 ) / 2 == 1600
 	if nativeSize > 1067 {
 		nativeBitrate = "1.8M"
 		fmt.Println("need a 480p encode")
-		if err = EncodeToFormat(ws, filename, "mp4", "h264", "0.8M", "0x0"); err != nil {
+		downscaleResolution = fmt.Sprintf("854x%0.f", 854*aspectRatio)
+		fmt.Println(downscaleResolution)
+		if err = EncodeToFormat(ws, filename, "-480p", "0.8M", downscaleResolution); err != nil {
+			return
+		}
+	}
+
+	// Halfway between 1080p and 720p
+	// ( 1920 + 1280 ) / 2 == 1600
+	if nativeSize > 1600 {
+		nativeBitrate = "4M"
+		fmt.Println("need a 720p encode")
+		downscaleResolution = fmt.Sprintf("1280x%0.f", 1280*aspectRatio)
+		fmt.Println(downscaleResolution)
+		if err = EncodeToFormat(ws, filename, "-720p", "1.8M", downscaleResolution); err != nil {
+			return
+		}
+	}
+
+	// Halfway between 1440p and 1080p
+	// ( 2560 + 1920 ) / 2 == 2240
+	if nativeSize > 2240 {
+		nativeBitrate = "7.2M"
+		fmt.Println("need a 1080p encode")
+		downscaleResolution = fmt.Sprintf("1920x%0.f", 1920*aspectRatio)
+		fmt.Println(downscaleResolution)
+		if err = EncodeToFormat(ws, filename, "-1080p", "4M", downscaleResolution); err != nil {
+			return
+		}
+	}
+
+	// Halfway between 4k and 1440p
+	// ( 3840 + 2560 ) / 2 == 3200
+	if nativeSize > 3200 {
+		nativeBitrate = "16.6M"
+		fmt.Println("need a 1440p encode")
+		downscaleResolution = fmt.Sprintf("2560x%0.f", 2560*aspectRatio)
+		fmt.Println(downscaleResolution)
+		if err = EncodeToFormat(ws, filename, "-1440p", "7.2M", downscaleResolution); err != nil {
 			return
 		}
 	}
@@ -100,26 +139,15 @@ func Encode(w http.ResponseWriter, r *http.Request) {
 	if nativeSize > 5760 {
 		nativeBitrate = "66M"
 		fmt.Println("need a 4k encode")
+		downscaleResolution = fmt.Sprintf("3840x%0.f", 3840*aspectRatio)
+		fmt.Println(downscaleResolution)
+		if err = EncodeToFormat(ws, filename, "-2160p", "16.6M", downscaleResolution); err != nil {
+			return
+		}
 	}
 
-	// Halfway between 4k and 1440p
-	// ( 3840 + 2560 ) / 2 == 3200
-	if nativeSize > 3200 {
-		nativeBitrate = "10M"
-		fmt.Println("need a 1440p encode")
-	}
-
-	// Halfway between 1440p and 1080p
-	// ( 2560 + 1920 ) / 2 == 2240
-	if nativeSize > 2240 {
-		nativeBitrate = "10M"
-		fmt.Println("need a 1080p encode")
-	}
-
-	// Halfway between 1080p and 720p
-	// ( 1920 + 1280 ) / 2 == 1600
-	if nativeSize > 1600 {
-		fmt.Println("need a 720p encode")
+	if err = EncodeToFormat(ws, filename, "", "16.6M", fmt.Sprintf("%0.fx%0.f", x, y)); err != nil {
+		return
 	}
 
 	fmt.Println(nativeBitrate)
@@ -127,7 +155,7 @@ func Encode(w http.ResponseWriter, r *http.Request) {
 }
 
 // EncodeToFormat encodes a video source and sends back progress via the websocket
-func EncodeToFormat(ws *websocket.Conn, filename string, format string, codec string, bitrate string, size string) error {
+func EncodeToFormat(ws *websocket.Conn, filename string, suffix string, bitrate string, size string) error {
 	// Set up our encoding options
 	ffmpegConf := &ffmpeg.Config{
 		FfmpegBinPath:   "/usr/local/bin/ffmpeg",
@@ -136,6 +164,8 @@ func EncodeToFormat(ws *websocket.Conn, filename string, format string, codec st
 	}
 
 	overwrite := true
+	format := "mp4"
+	codec := "h264"
 
 	opts := ffmpeg.Options{
 		OutputFormat: &format,
@@ -145,19 +175,22 @@ func EncodeToFormat(ws *websocket.Conn, filename string, format string, codec st
 		Resolution:   &size,
 	}
 
-	ws.WriteMessage(websocket.TextMessage, []byte("Starting webm encode"))
+	ws.WriteMessage(websocket.TextMessage, []byte("Starting h264 encode"))
 	progress, err := ffmpeg.
 		New(ffmpegConf).
 		Input(fmt.Sprintf("files/temp-videos/%v", filename)).
-		Output(fmt.Sprintf("files/videos/%v.webm", strings.TrimSuffix(filename, filepath.Ext(filename)))).
+		Output(fmt.Sprintf("files/videos/%v%v.mp4", strings.TrimSuffix(filename, filepath.Ext(filename)), suffix)).
 		WithOptions(opts).
 		Start(opts)
 
+	fmt.Println("hmm")
 	if err != nil {
 		fmt.Println(err)
+		fmt.Println("shit son")
 		ws.WriteMessage(websocket.TextMessage, []byte("Error encoding webm"))
 		return err
 	}
+	fmt.Println("k")
 
 	for msg := range progress {
 		fmt.Println(msg.GetProgress())
